1029dd56aa69ade1df41858eb457e551658fdfa6
VitorInacioBorges <vitorinacioborges@gmail.com>
2025-07-23 09:34:33 -0300

    vitor commit

--- Stock.js
+++ Stock.js
@@ -45,3 +45,39 @@ switch (menu){
 }
 })
 }
+
+function Remover(){
+    if(estoque.length == 0){
+        console.log('Estoque vazio!');
+        MenuPrincipal();
+    } else {
+        console.log('============ PRODUTOS LISTADOS ============');
+        for(let i = 0; i<estoque.length; i++){
+            console.log(`${i + 1} - Nome: ${estoque[i].Nome}, Quantidade: ${estoque[i].Quantidade}, Preço: ${estoque[i].Preco}`);
+        };
+        rl.question('Escolha o número do produto que deseja remover: ', (input) => {
+            const index = parseInt(input) - 1;
+            if(isNaN(index) || index < 0 || index > estoque.length){
+                console.log('Entrada inválida!! Volte ao menu!');
+                MenuPrincipal();
+            } else {
+                const [removido] = estoque.splice(index);
+                rl.question('', MenuPrincipal);
+                console.log('Produto removido com sucesso!!');
+            }
+        });
+    }
+}
+
+function VerificarEstoque(){
+    let baixoEstoque = 0;
+    console.log('Os seguintes produtos possuem quantidade baixa (menor que 5):');
+    for(let i = 0; i < estoque.length; i++){
+        if(estoque[i].Quantidade < 5){
+            console.log(`Nome: ${estoque[i].Nome}, Quantidade: ${estoque[i].Quantidade}`);
+            baixoEstoque++;
+        }
+    }
+    console.log(`No total temos ${baixoEstoque} produtos com quantidade baixa!\n Aperte ENTER para voltar ao menu...`);
+    rl.question('', MenuPrincipal);
+}
